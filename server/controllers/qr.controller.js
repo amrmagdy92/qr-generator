3bcc60e12d01b81ca330fc7a584e8f7825ba2ab2
unknown <amr.magdy.ahmed92@gmail.com>
2024-02-24 16:59:23 +0200

    Imported the required modules and specified the logic sequence of the generateQR function

---
+++ server/controllers/qr.controller.js
@@ -0,0 +1,14 @@
+import qrGenerator from "@nimashoghi/qr-code-styling"
+import nodeCanvas from "canvas"
+import { optionsValidator } from "../helpers/qr.helper"
+
+const generateQR = (options) => {
+    // TODO:
+    // 1. validate incoming options
+    // 2. create qr code from the generation module
+    // 3. return the generated qr code
+}
+
+export {
+    generateQR
+}

fdc14d75f7d4e60837b225189d8cbb1284520a7d
unknown <amr.magdy.ahmed92@gmail.com>
2024-02-25 01:34:09 +0200

    implemented generateQR

--- server/controllers/qr.controller.js
+++ server/controllers/qr.controller.js
@@ -1,12 +1,30 @@
+import fs from "fs"
 import qrGenerator from "@nimashoghi/qr-code-styling"
 import nodeCanvas from "canvas"
 import { optionsValidator } from "../helpers/qr.helper"
 
 const generateQR = (options) => {
-    // TODO:
-    // 1. validate incoming options
-    // 2. create qr code from the generation module
-    // 3. return the generated qr code
+    return new Promise((resolve, reject) => {
+        let validationErrors = optionsValidator(options)
+        if (Object.keys(validationErrors).length > 0) {
+            reject({
+                code: 400,
+                msg: validationErrors
+            })
+        } else {
+            let nodeOptions = {nodeCanvas, ...options}
+            let generatedQR = new qrGenerator(nodeOptions).download({ buffer: true })
+            .then((buffer) => {
+                let qrName = `qr_${Date.now()}.png`
+                fs.writeFileSync(qrName, buffer)
+                let file = fs.readFileSync(qrName)
+                resolve({
+                    code: 200,
+                    msg: file
+                })
+            })
+        }
+    })
 }
 
 export {

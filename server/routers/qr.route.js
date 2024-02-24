304510f5a18d49be8473c2c9c4f0fa3c13a30dd4
unknown <amr.magdy.ahmed92@gmail.com>
2024-02-25 01:32:18 +0200

    Implemented Route logic

---
+++ server/routers/qr.route.js
@@ -0,0 +1,21 @@
+import { Router } from "express"
+import { generateQR } from "../controllers/qr.controller"
+
+const router = Router()
+
+router.route('/')
+    .post((request, response) => {
+        generateQR(request.body.options)
+        .then(result => {
+            let responseCode = result.code
+            let data = result.msg
+            response.status(responseCode).json({ msg: data })
+        })
+        .catch( error => {
+            let responseCode = error.code
+            let errorMessage = error.msg
+            response.status(responseCode).json({ msg: errorMessage })
+        })
+    })
+
+export default router

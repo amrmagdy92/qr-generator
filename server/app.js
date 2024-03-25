f949c42b6d5996b642eb5ce3cd386f074de0fa4f
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-03-25 21:34:26 +0200

    updated app.js to remove credentials from CORS configuration

--- server/app.js
+++ server/app.js
@@ -32,8 +32,7 @@ const configuredHelmet = helmet({
 })
 const configuredCors = cors({
     origin: process.env.CORS_ORIGIN,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 })
 const configuredRateLimiter = expressRateLimiter({
     window: 30*1000,
